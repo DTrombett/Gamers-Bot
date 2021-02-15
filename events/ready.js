804485c05d860b116ce6b949e15f0391d5ad01c0
DTrombett <maxtromb.dt@gmail.com>
2021-02-15 15:41:09 +0100

    Version 5.0.0

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = (client, id, guilds) => {
   try {
     console.log(`Shard ${id} ready with ${!!guilds ? guilds.size : 0} guilds unavailable.`);
     client.customAvatar = client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
-    client.webhook = new WebhookClient('797175956158283837', process.env.WEBHOOKTOKEN);
+    client.webhook = new WebhookClient('810879034241974353', process.env.WEBHOOK_TOKEN);
     client.error = require('../config/error.js');
     require('../config/functions.js')(client);
     require('../config/variables.js')(client);
