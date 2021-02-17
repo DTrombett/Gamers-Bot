713f11256ab8b6c46a497c02b88bf785cb6b81d3
DTrombett <maxtromb.dt@gmail.com>
2021-02-17 11:08:45 +0100

    Version 5.1.1

--- config/Command.js
+++ config/Command.js
@@ -1,15 +1,91 @@
 const { Collection } = require('discord.js');
 
-module.exports = class Command {
-  constructor(object) {
-    this.name = object.name;
-    this.description = object.description;
-    this.help = object.help || '';
-    this.usage = object.usage || '';
-    this.aliases = object.aliases || [];
-    this.examples = object.examples ? object.examples.map(t => this.name + t) : [this.name];
-    this.execute = object.execute || (() => { });
-    this.time = object.time || 1000;
-    this.cooldown = new Collection();
-  }
+/**
+ * Represents a command of the bot.
+ */
+class Command {
+    /**
+     * @param {name} String The name of this command
+     * @param {callback} [Function=()=>{}] The callback to execute when this command is called
+     */
+    constructor(name, callback = () => { }) {
+        /**
+         * Name of the command
+         * @type {String}
+         */
+        this.name = name;
+
+        /**
+         * The callback of the command
+         * @type {Function}
+         */
+        this.execute = callback;
+
+        /**
+         * The cooldown in milliseconds of the command
+         * @type {number}
+         */
+        this.time = 1000;
+
+        /**
+         * Users in cooldown
+         * @type {Collection<Snowflake, User>}
+         */
+        this.cooldown = new Collection();
+
+        /**
+         * The help message for this command
+         * @type {String}
+         */
+        this.help = '';
+
+        /**
+         * The usage of this command
+         * @type {String}
+         */
+        this.usage = '';
+
+        /**
+         * Aliases for the command
+         * @type {Array}
+         */
+        this.aliases = [];
+
+        /**
+         * Example of this command
+         * @type {Array}
+         */
+        this.examples = [];
+    }
+
+    /**
+     * Add new aliases for this command.
+     * @param  {...String} args Aliases to add
+     * @returns {Command}
+     * @example
+     * // Add the alias "test"
+     * command.addAlias('test');
+     * @example
+     * // Add more than one alias
+     * command.addAlias('test', 'echo');
+     */
+    addAlias(...args) {
+        Array.isArray(this.aliases) ? args.forEach(alias => this.aliases.push(alias)) : this.aliases = args;
+        return this;
+    }
+
+    /**
+     * Add examples of the command
+     * @param  {...any} args Examples to add without command name
+     * @returns {Command}
+     * @example
+     * // Add an example to the command
+     * command.addExample("Hello world!");
+     */
+    addExample(...args) {
+        Array.isArray(this.examples) ? args.forEach(ex => this.examples.push(this.name + ex)) : this.examples = args.map(t => `${this.name} ${t}`);
+        return this;
+    }
 };
+
+module.exports = Command;
--- index.js
+++ index.js
@@ -9,48 +9,50 @@ console.log('Starting...');
 // Starts declaring constants
 
 const { Client, Collection } = require('discord.js');
+
+// Configure the dotenv module
 require('dotenv').config();
+
+// Create a new Client
 const client = new Client({
-  partials: ['MESSAGE', 'REACTION', 'USER', 'GUILD_MEMBER'],
-  messageCacheMaxSize: 500,
-  messageCacheLifetime: 86400,
-  messageSweepInterval: 3600,
-  messageEditHistoryMaxSize: 10,
-  fetchAllMembers: true,
-  disableMentions: 'everyone',
-  restWsBridgeTimeout: 36000,
-  restTimeOffset: 100,
-  restRequestTimeout: 10000,
-  presence: {
-    status: 'online',
-    activity: {
-      name: '-help',
-      type: 'LISTENING',
+    partials: ['MESSAGE', 'REACTION', 'USER', 'GUILD_MEMBER'],
+    messageCacheMaxSize: 500,
+    messageCacheLifetime: 86400,
+    messageSweepInterval: 3600,
+    messageEditHistoryMaxSize: 10,
+    fetchAllMembers: true,
+    disableMentions: 'everyone',
+    restWsBridgeTimeout: 36000,
+    restTimeOffset: 100,
+    restRequestTimeout: 10000,
+    presence: {
+        status: 'online',
+        activity: {
+            name: '-help',
+            type: 'LISTENING',
+        }
+    },
+    ws: {
+        large_threshold: 100
     }
-  },
-  ws: {
-    large_threshold: 100
-  }
 });
 const { readdirSync } = require('fs');
+
+// Import all files from folders
 const commandFiles = readdirSync('./commands/').filter(file => {
-  return file.endsWith('.js');
+    return file.endsWith('.js');
 });
 const eventFiles = readdirSync('./events/').filter((event) => {
-  return event.endsWith('.js');
+    return event.endsWith('.js');
 });
 const automod = readdirSync('./automod/').filter(file => {
-  return file.endsWith('.js');
+    return file.endsWith('.js');
 });
 const express = require('express');
 const app = express();
 const port = 3000;
 const { inspect } = require('util');
 const Command = require('./config/Command.js');
-
-client.var = (name) => {
-  return require(`./variables/${name}.json`);
-};
 
 console.log('Constants declared');
 
@@ -62,120 +64,123 @@ client.slashCommands = new Collection();
 client.cooldown = new Collection();
 client.automod = new Collection();
 
+// Handle the files in correct property
 for (let file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, new Command(command));
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, new Command(command));
 }
 for (let file of eventFiles) {
-  const event = require(`./events/${file}`);
-  const eventName = file.split('.')[0];
-  client.events.set(eventName, event);
+    const event = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
+    client.events.set(eventName, event);
 }
 for (let file of automod) {
-  const automodFile = require(`./automod/${file}`);
-  client.automod.set(automodFile.name, automodFile);
+    const automodFile = require(`./automod/${file}`);
+    client.automod.set(automodFile.name, automodFile);
 }
 
 console.log('All the commands are ready!');
 
 // Track all the events
 
+// Process events
 process.on('unhandledRejection', (reason, promise) => {
-  return console.log('Promise rejected! Reason: ', reason);
+    return console.log('Promise rejected! Reason: ', reason);
 });
 process.on('beforeExit', (code) => {
-  return console.log('Process exiting with code: ', code);
+    return console.log('Process exiting with code: ', code);
 });
 process.on('exit', (code) => {
-  return console.log('Process exit with code: ', code);
+    return console.log('Process exit with code: ', code);
 });
 process.on('disconnect', code => {
-  return console.log('Process disconnected with code: ', code);
+    return console.log('Process disconnected with code: ', code);
 });
 process.on('message', (message, sendHandle) => {
-  return console.log('Process received a message! ', message, sendHandle);
+    return console.log('Process received a message! ', message, sendHandle);
 });
 process.on('multipleResolves', (type, promise, reason) => {
-  return console.error('A promise was resolved more than once! Type: ', type, ' Reason: ', reason);
+    return console.error('A promise was resolved more than once! Type: ', type, ' Reason: ', reason);
 });
 process.on('uncaughtException', (err, origin) => {
-  return console.log('Error occurred! Error: ', err, ' Origin: ', origin);
+    return console.log('Error occurred! Error: ', err, ' Origin: ', origin);
 });
 process.on('uncaughtExceptionMonitor', (err, origin) => {
-  return console.log('Error ', err, origin);
+    return console.log('Error ', err, origin);
 });
 process.on('warning', (warning) => {
-  return console.warn(warning);
+    return console.warn(warning);
 });
 
+// Client events
 client.once('ready', () => {
-  return console.log(`Logged in as ${client.user.tag}`);
+    return console.log(`Logged in as ${client.user.tag}`);
 });
 client.on('shardReady', (id, unavailableGuilds) => {
-  return client.events.get('ready')(client, id, unavailableGuilds);
+    return client.events.get('ready')(client, id, unavailableGuilds);
 });
 client.once('error', error => {
-  return console.error('Discord error occurred! Error: ', error);
+    return console.error('Discord error occurred! Error: ', error);
 });
 client.on('shardError', (error, shardID) => {
-  return console.error(`Received an error on shard ${shardID} `, `Message: ${error}`);
+    return console.error(`Received an error on shard ${shardID} `, `Message: ${error}`);
 });
 client.on('shardResume', (id, replayedEvents) => {
-  return client.events.get('resume')(client, id, replayedEvents);
+    return client.events.get('resume')(client, id, replayedEvents);
 });
 client.once('invalidated', () => {
-  return process.exit('Bot session invalidated!');
+    return process.exit('Bot session invalidated!');
 });
 client.on('shardReconnecting', (id) => {
-  return client.events.get('reconnect')(client, id);
+    return client.events.get('reconnect')(client, id);
 });
 client.on('shardDisconnect', (event, id) => {
-  return client.events.get('disconnect')(client, event, id);
+    return client.events.get('disconnect')(client, event, id);
 });
 client.on('message', message => {
-  return client.events.get('message')(message, client);
+    return client.events.get('message')(message, client);
 });
 client.on('debug', info => {
-  return client.events.get('debug')(info, client);
+    return client.events.get('debug')(info, client);
 });
 client.on('warn', info => {
-  return console.warn(`WARNING: ${info}`);
+    return console.warn(`WARNING: ${info}`);
 });
 client.on('guildCreate', guild => {
-  return console.log(`Bot joined a new guild! Guild: ${inspect(guild)}`);
+    return console.log(`Bot joined a new guild! Guild: ${inspect(guild)}`);
 });
 client.on('guildDelete', guild => {
-  return console.log(`Bot left a guild! Guild: ${inspect(guild)}`);
+    return console.log(`Bot left a guild! Guild: ${inspect(guild)}`);
 });
 client.on('guildUnavailable', guild => {
-  return console.log(`A guild is unavailable! Guild: ${inspect(guild)}`);
+    return console.log(`A guild is unavailable! Guild: ${inspect(guild)}`);
 });
 client.on('rateLimit', rateLimitInfo => {
-  return console.log(`Bot hitted rate limit! Info: ${inspect(rateLimitInfo)}`);
+    return console.log(`Bot hitted rate limit! Info: ${inspect(rateLimitInfo)}`);
 });
 client.on('messageDelete', message => {
-  return client.events.get('messageDelete')(message, client);
+    return client.events.get('messageDelete')(message, client);
 });
 client.on('messageDeleteBulk', messages => {
-  return client.events.get('messageDeleteBulk')(messages, client);
+    return client.events.get('messageDeleteBulk')(messages, client);
 });
 client.on('channelCreate', channel => {
-  return client.events.get('channelCreate')(channel, client);
+    return client.events.get('channelCreate')(channel, client);
 });
 client.on('channelDelete', channel => {
-  return client.events.get('channelDelete')(channel, client);
+    return client.events.get('channelDelete')(channel, client);
 });
 client.on('emojiDelete', emoji => {
-  return client.events.get('emojiDelete')(emoji, client);
+    return client.events.get('emojiDelete')(emoji, client);
 });
 client.on('messageUpdate', (oldMessage, newMessage) => {
-  return client.events.get('messageUpdate')(oldMessage, newMessage, client);
+    return client.events.get('messageUpdate')(oldMessage, newMessage, client);
 });
 client.on('guildBanAdd', (guild, user) => {
-  return client.events.get('guildBanAdd')(guild, user, client);
+    return client.events.get('guildBanAdd')(guild, user, client);
 });
 client.on('guildMemberAdd', member => {
-  return client.events.get('guildMemberAdd')(member, client);
+    return client.events.get('guildMemberAdd')(member, client);
 });
 
 console.log('Events loaded! Starting to connect...');
@@ -183,23 +188,23 @@ console.log('Events loaded! Starting to connect...');
 // Log the bot in and initialize the website
 
 app.use((req, res, next) => {
-  var path = req.url.split('/');
-  path.shift();
-  if (req.headers.referer !== 'https://gamersbot.dtrombett.repl.co/' || !req.headers.referer.startsWith(req.headers.origin) || path[0] != 'api') return res.redirect('https://gamersbot.dtrombett.repl.co');
-  path.shift();
-  res.set('Access-Control-Allow-Origin', 'https://gamersbot.dtrombett.repl.co');
-  var response;
-  try {
-    response = require(`./server/${path[0]}.js`)(req, path, client);
-  } catch (err) {
-    res.status(404).send();
-    console.error(err);
-  }
-  if (!response) return;
-  res.send(JSON.stringify(response));
+    var path = req.url.split('/');
+    path.shift();
+    if (req.headers.referer !== 'https://gamersbot.dtrombett.repl.co/' || !req.headers.referer.startsWith(req.headers.origin) || path[0] != 'api') return res.redirect('https://gamersbot.dtrombett.repl.co');
+    path.shift();
+    res.set('Access-Control-Allow-Origin', 'https://gamersbot.dtrombett.repl.co');
+    var response;
+    try {
+        response = require(`./server/${path[0]}.js`)(req, path, client);
+    } catch (err) {
+        res.status(404).send();
+        console.error(err);
+    }
+    if (!response) return;
+    res.send(JSON.stringify(response));
 });
 app.listen(port, () => {
-  return console.log(`Listening at http://localhost:${port}`);
+    return console.log(`Listening at http://localhost:${port}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
